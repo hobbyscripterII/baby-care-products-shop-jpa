acfed1549b70632e257a5d65f6812057e5907894
hobbyscripterII <wruomma2@naver.com>
2024-02-16 21:40:27 +0900

    feat : 주문, 상품, 환불, 리뷰(진행 중) entity 생성

---
+++ src/main/java/com/baby/babycareproductsshop/entity/order_jy/OrderEntity.java
@@ -0,0 +1,54 @@
+package com.baby.babycareproductsshop.entity.order_jy;
+
+import com.baby.babycareproductsshop.entity.BaseEntity;
+import com.baby.babycareproductsshop.entity.user.UserAddressEntity;
+import com.baby.babycareproductsshop.entity.user.UserEntity;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.ColumnDefault;
+
+@Data
+@Entity
+@Table(name = "t_order")
+public class OrderEntity extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "BIGINT UNSIGNED")
+    private Long iorder;
+
+    @ManyToOne
+    @JoinColumn(name = "iuser", nullable = false)
+    private UserEntity userEntity;
+
+    @ManyToOne
+    @JoinColumn(name = "iaddress", nullable = false)
+    private UserAddressEntity userAddressEntity;
+
+    @ManyToOne
+    @JoinColumn(name = "ipayment_option", nullable = false)
+    @ColumnDefault("'1'")
+    private OrderPaymentOptionEntity orderPaymentOptionEntity;
+
+    @Column(length = 20)
+    private String address_nm;
+
+    @Column(length = 13)
+    private String phoneNumber;
+
+    @Column(length = 50)
+    private String email;
+
+    @Column(columnDefinition = "TINYINT UNSIGNED", length = 20, nullable = false)
+    private int processState;
+
+    @Column(columnDefinition = "INT UNSIGNED")
+    @ColumnDefault("0")
+    private int totalPrice;
+
+    @Column(length = 100)
+    private String deliveryRequest;
+
+    @Column(length = 11, columnDefinition = "INT UNSIGNED")
+    @ColumnDefault("0")
+    private int deleteFl;
+}
---
+++ src/main/java/com/baby/babycareproductsshop/entity/refund_jy/RefundEntity.java
@@ -0,0 +1,34 @@
+package com.baby.babycareproductsshop.entity.refund_jy;
+
+import com.baby.babycareproductsshop.entity.BaseEntity;
+import com.baby.babycareproductsshop.entity.order_jy.OrderDetailsEntity;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.ColumnDefault;
+
+@Data
+@Entity
+@Table(name = "t_refund")
+public class RefundEntity extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "BIGINT UNSIGNED")
+    private Long irefund;
+
+    @ManyToOne
+    @JoinColumn(name = "idetails", columnDefinition = "BIGINT UNSIGNED", nullable = false)
+    private OrderDetailsEntity orderDetailsEntity;
+
+    @Column(length = 200, nullable = false)
+    private String contents;
+
+    @Column(length = 11, nullable = false)
+    private int refundCnt;
+
+    @Column(length = 11, nullable = false)
+    private int refundPrice;
+
+    @Column(length = 4, columnDefinition = "TINYINT UNSIGNED", nullable = false)
+    @ColumnDefault("'0'")
+    private int complateFl;
+}
