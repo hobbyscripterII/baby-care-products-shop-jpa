627059cd0c6642132bf849771b7246ca5d52f805
TH <kareme007@gmail.com>
2024-02-16 17:35:17 +0900

    refactor : BaseEntity

--- src/main/java/com/baby/babycareproductsshop/entity/BaseEntity.java
+++ src/main/java/com/baby/babycareproductsshop/entity/BaseEntity.java
@@ -1,6 +1,7 @@
 package com.baby.babycareproductsshop.entity;
 
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -8,6 +9,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 
 @Data
+@MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseEntity extends CreatedAtEntity{
     @LastModifiedDate
