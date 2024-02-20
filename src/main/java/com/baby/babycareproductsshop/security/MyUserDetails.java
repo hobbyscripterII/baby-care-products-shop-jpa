4cadcfa64862ce0b8e97ff282e6feae99869cc42
TH <kareme007@gmail.com>
2024-02-20 16:08:09 +0900

    refactor : admin user & email

--- src/main/java/com/baby/babycareproductsshop/security/MyUserDetails.java
+++ src/main/java/com/baby/babycareproductsshop/security/MyUserDetails.java
@@ -1,9 +1,11 @@
 package com.baby.babycareproductsshop.security;
 
 
+import com.baby.babycareproductsshop.entity.user.UserEntity;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
@@ -13,10 +15,16 @@ import java.util.Collection;
 public class MyUserDetails implements UserDetails {
 
     private MyPrincipal myPrincipal;
+    private UserEntity userEntity;
 
     @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {    //권한
-        return null;
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (myPrincipal == null) {
+            return null;
+        }
+        return this.myPrincipal.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                .toList();
     }
 
     @Override
@@ -25,8 +33,8 @@ public class MyUserDetails implements UserDetails {
     }
 
     @Override
-    public String getUsername() {   //시큐리티 루틴 이용하면 구현 필요
-        return null;
+    public String getUsername() {   //시큐리티 루틴 이용하면 구현 필요. 소셜 로그인 하면 사용하는 듯 ?
+        return userEntity == null ? null : userEntity.getUid();
     }
 
     @Override
