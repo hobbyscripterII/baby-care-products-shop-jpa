ab028740c1e646460797e6bc37844c4aa136a049
hobbyscripterII <wruomma2@naver.com>
2024-02-27 10:49:28 +0900

    memo : 스케줄러 어노테이션 추가

--- src/main/java/com/baby/babycareproductsshop/BabyCareProductsShopApplication.java
+++ src/main/java/com/baby/babycareproductsshop/BabyCareProductsShopApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.web.config.PageableHandlerMethodArgumentResolverCustomizer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling // 스케줄러
 @EnableJpaAuditing
 @SpringBootApplication
 @ConfigurationPropertiesScan
